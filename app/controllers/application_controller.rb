54ae062afac35e66ada9d1910bdf31b993585582
Matthew Beebe <grow.food.everywhere@gmail.com>
2017-01-02 18:33:17 -0500

    more styling changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :birthdate])
-  	devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :birthdate, :solPoints])
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :solPoints])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161225181046) do
+ActiveRecord::Schema.define(version: 20161230170045) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20161225181046) do
     t.datetime "updated_at",                          null: false
     t.string   "username"
     t.string   "name"
-    t.date     "birthdate"
     t.integer  "solPoints"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
